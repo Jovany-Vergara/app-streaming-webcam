afc53ca07f92ab5d9b5d7be60b60e9cce90f5b22
Jovany Vergara <diego.jobany11@gmail.com>
2024-03-07 15:49:53 -0600

    agregando el package y poniendo comentarios en el codigo js

--- js/main.js
+++ js/main.js
@@ -1,30 +1,44 @@
+//Esta línea establece una conexión con el servidor Socket.IO en el mismo origen (URL) que la página web.
 const socket = io.connect(window.location.origin);
+//Esta línea selecciona el elemento de video con el ID 'webcam' en la página HTML.
 const webcam = document.querySelector('#webcam');
+//Esta línea selecciona los botones de inicio y parada en la página HTML.
 const startButton = document.querySelector('#start');
+//Esta línea selecciona el botón de detener con el ID 'stop' en la página HTML.
 const stopButton = document.querySelector('#stop');
+//Esta línea selecciona el elemento de video con el ID 'stream' en la página HTML.
 const stream = document.querySelector('#stream');
+//Esta línea declara una variable para almacenar el intervalo de tiempo que se usará para emitir imágenes de la webcam.
 let streamInterval;
 
 // Crear un canvas y obtener su contexto
 const canvas = document.createElement('canvas');
+// Obtener el contexto del canvas
 const context = canvas.getContext('2d');
 
+// Esta función inicia la emisión de la webcam.
 function startWebcamStream() {
+  // Comprobar si el navegador es compatible con la API de getUserMedia
   if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    //Esta línea solicita acceso a la cámara web del usuario. El objeto { video: true } indica que queremos un stream de video.
     navigator.mediaDevices.getUserMedia({ video: true })
+    //Si el usuario concede el acceso a la cámara web, el navegador devuelve una promesa que se resuelve con un objeto MediaStream
       .then((stream) => {
+        //sta línea establece el stream de la cámara web como la fuente del elemento de video en la página web.
         webcam.srcObject = stream;
+        //Esta línea agrega un manejador de eventos que comienza a reproducir el video cuando los metadatos del video (como la duración y las dimensiones del video) se han cargado.
         webcam.onloadedmetadata = (e) => {
           webcam.play();
         };
       })
+      //Si el usuario no concede el acceso a la cámara web, el navegador devuelve una promesa que se rechaza con un error.
       .catch((error) => {
         console.error("Error accessing webcam: ", error);
       });
   }
 
+  // Cuando el video comienza a reproducirse, ejecutar el siguiente código
   webcam.addEventListener('play', () => {
-    // Asegurar que el tamaño del canvas coincide con el tamaño del video
     canvas.width = webcam.videoWidth;
     canvas.height = webcam.videoHeight;
 
@@ -37,6 +51,7 @@ function startWebcamStream() {
   });
 }
 
+// Esta función detiene la emisión de la webcam.
 function stopWebcamStream() {
   // Detener la emisión de la webcam
   clearInterval(streamInterval);
--- server.js
+++ server.js
@@ -1,21 +1,31 @@
+// Importa el módulo express, que proporciona utilidades para construir servidores web
 const express = require('express');
+// Crea una nueva aplicación express
 const app = express();
+// Crea un nuevo servidor HTTP que delega las solicitudes a la aplicación express
 const server = require('http').Server(app);
+// Crea un nuevo servidor Socket.IO que se adjunta al servidor HTTP
 const io = require('socket.io')(server);
 
+// Sirve archivos estáticos (como HTML, CSS y JS) desde el directorio actual
 app.use(express.static(__dirname));
 
+// Define una ruta para la raíz del sitio web ("/") que envía el archivo index.html al cliente
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+// Inicia el servidor en el puerto 3001 y registra un mensaje en la consola cuando el servidor ha iniciado
 server.listen(3001, () => {
   console.log('Listening on localhost:3001');
 });
 
+// Registra un manejador de eventos que se activa cuando un cliente se conecta al servidor Socket.IO
 io.on('connection', (socket) => {
   console.log('A user connected');
 
+  // Registra un manejador de eventos que se activa cuando el cliente emite un evento 'stream'
+  // Este evento asume que el cliente envía una imagen, que luego se emite a todos los clientes conectados
   socket.on('stream', (image) => {
     io.emit('stream', image);
   });
